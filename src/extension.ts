3f3a5544232596d431998b8f93eec6f33f8b8dd4
David Kanekanian <dkanekanian1@gmail.com>
2025-06-29 20:11:35 +0100

    prepare for publish

--- src/extension.ts
+++ src/extension.ts
@@ -4,19 +4,19 @@ import { WidgetsTreeDataProvider } from "./widgetsTreeData";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  const elementsTreeDataProvider = new WidgetsTreeDataProvider(context);
+  const widgetsTreeDataProvider = new WidgetsTreeDataProvider(context);
 
   context.subscriptions.push(
     vscode.window.registerTreeDataProvider(
-      "qt-debug-tools.elements",
-      elementsTreeDataProvider
+      "qt-debug-tools.widgets",
+      widgetsTreeDataProvider
     )
   );
 
   // Register a command to refresh the tree view
   context.subscriptions.push(
     vscode.commands.registerCommand("qt-debug-tools.refresh", () => {
-      elementsTreeDataProvider.refresh();
+      widgetsTreeDataProvider.refresh();
     })
   );
 }
